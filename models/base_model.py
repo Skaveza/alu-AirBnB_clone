636ac3e668f3ed663fbb1e27074762fdf66a23be
Skaveza <s.mwachoni@alustudent.com>
2024-01-10 13:20:35 +0300

    updated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -8,12 +8,12 @@ from datetime import datetime
 
 class BaseModel:
    def __init__(self):
-       self.id = str(uuid.uuid4()()
+       self.id = str(uuid.uuid4())
  
        self.created_at = datetime.utcnow()
        self.updated_at = datetime.utcnow()
  
-   def save (self)
+   def save (self):
        """
       Updates the updated_at attribute with a new value
        """
@@ -30,9 +30,23 @@ class BaseModel:
 
        return inst_dict
 
-   def ___str___(self):
+   def __str__(self):
        """
        String representation of the instance class
        """
-       class_name = self.__class__.name__
-       return f " [{class_name}] ({self.id}) {self.__dict__}"
+       class_name = self.__class__.__name__
+       return f"[{class_name}] ({self.id}) {self.__dict__}"
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
