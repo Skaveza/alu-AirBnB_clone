1f2015065c02693715c436a0828c1b952e6b9973
Skaveza <s.mwachoni@alustudent.com>
2024-01-19 22:11:19 +0200

    updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,73 +4,53 @@ from models.base_model import BaseModel
 
 
 class FileStorage:
-   """
+    """
+    This class manages the serialization and deserialization of instances
+    to and from JSON.
+    """
+    __file_path = "file.json"
+    __objects = {}
 
-   """
-   __file_path = "file.json"
- 
-  __objects = {}
+    def new(self, obj):
+        """
+        Add a new object to the dictionary __objects.
+        """
+        obj_cls_name = obj.__class__.__name__
+        key = "{}.{}".format(obj_cls_name, obj.id)
+        FileStorage.__objects[key] = obj
 
-  def new(self, obj):
-      """
+    def all(self):
+        """
+        Return the dictionary __objects.
+        """
+        return FileStorage.__objects
 
-      """
-      obj_cls_name = obj.__class__.__name__
+    def save(self):
+        """
+        Serialize __objects to the JSON file.
+        """
+        all_objs = FileStorage.__objects
+        obj_dict = {}
 
-      key = "{}.{}".format(obj_cls_name, obj.id)
+        for obj_key in all_objs.keys():
+            obj_dict[obj_key] = all_objs[obj_key].to_dict()
 
-      FileStorage.__objects[key] = obj
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            json.dump(obj_dict, file)
 
-  def all(self):
-      """
+    def reload(self):
+        """
+        Deserialize the JSON file to __objects.
+        """
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+                try:
+                    obj_dict = json.load(file)
 
-      """
-      return FileStorage.__objects
-
-  def save(self):
-      """
-      
-      """
-      all_objs = FileStorage.__objects
-      
-      obj_dict = {}
-
-      for obj in all_objs.keys():
-          obj_dict[obj] = all_objs[obj].to_dict()
-
-      with open(FileStorage.__file_path), "w", encoding="utf-8") as file:
-          json.dump(obj_dict, file)
-
-  def reload(self):
-      """
-
-      """
-      if os.path.isfile(FileStorage.__file_path):
-          with open(FileStorage.__file_path), "r", encoding="utf-8" as file:
-               try:
-                   obj_dict = json.load(file)
-        
-                   for key, value in obj_dict.items():
-                       class_name, obj_id = key.split('.')
-                       
-                       cls = eval(class_name)
-
-                       instance = cls(**values)
-
-                       FileStorage.__objects[key] = instance
-               except Exception:
-                   pass  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                    for key, values in obj_dict.items():
+                        class_name, obj_id = key.split('.')
+                        cls = eval(class_name)
+                        instance = cls(**values)
+                        FileStorage.__objects[key] = instance
+                except Exception:
+                    pass
